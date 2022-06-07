0161a073f2275451117678aff0cb82bdc98b80ce
Muhammad Usman <usmansheikh4949@gmail.com>
2022-06-08 01:36:08 +0500

    Final

--- reg.php
+++ reg.php
@@ -8,8 +8,6 @@ if($con){
     echo "not connect";
 }
 $fname=$_POST['fname'];
-$mname=$_POST['mname'];
-$lname=$_POST['lname'];
 $email=$_POST['email'];
 $password=$_POST['password'];
 $address=$_POST['address'];
@@ -26,24 +24,20 @@ $num = mysqli_num_rows($ch);
 if($num == 1){
     echo"use different username";
 }else{
-$sql="Insert into local(fname,mname,lname,email,password,address,college,mobile,gender,city,state,country)
-values ('$fname','$mname','$lname','$email','$password','$address','$college','$mobile','$gender','$city','$state','$country')";
-}
+$sql="INSERT INTO local (fname,email,password,address,college,mobile,country,state,city)
+ values ('$fname','$email','$password','$address','$college','$mobile','$country','$state','$city')";
 
 if (mysqli_query($con, $sql))
  {
-
+ header("location:display.php");
 	echo "New record created successfully";
 	echo "<a href='display.php'>Click for Display</a>          ";
 	echo "<a href='reg.html'>Back</a>";
 
-  } else {
+  } 
+  else {
 	echo "Error: " . $sql . "<br>" . mysqli_error($con);
   }
   
-
-
-
-	
-
+}
 ?>
